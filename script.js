dfca09863aaa5038aa16f437c19df02f8c2d3b09
palluri542 <praveensistk@gmail.com>
2015-08-30 22:23:27 +0530

    Update script.js
     row active selection

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
 var app = angular.module('myApp', []);
 
-app.controller('controller', function ($scope, $http, $filter) {
-
-
-    $http.get('clientList.json').then(function (res) {
-        $scope.clientList = res.data;
+app.factory("ClientService", function($http) {
+    return {
+        getFilesList: function() {
+           return $http.get('clientList.json');
+        },
+        getFileDetails: function() {
+            return $http.get('fileList.json');
+        }
+    };
+});
+app.controller('controller', function ($scope, ClientService) {
+    ClientService.getFilesList().success(function (res) {
+        $scope.clientList = res;
     });
     $scope.clientName = "NA";
     $scope.expected = "00";
     $scope.received = "00";
     $scope.failed = "00";
-    $scope.files = "00";
+    $scope.files = [];
+	$scope.SelectedclientID = 0;
 
     $scope.showDetails = function (clientID) {
         $scope.getSummary(clientID);
         $scope.getFilesList(clientID);
+		$scope.error =[];
+		
+		$scope.SelectedclientID = clientID;
+		
+		
     };
 
     $scope.getSummary = function (clientID) {
-        $http.get('clientList.json').success(function (client) {
-            $scope.expected = client[clientID - 1].expected;
-            $scope.received = client[clientID - 1].received;
-            $scope.failed = client[clientID - 1].failed;
+        ClientService.getFilesList().success(function (client) {
+			var clientFileDetails = client[clientID - 1];
+            $scope.expected = clientFileDetails.expected;
+            $scope.received = clientFileDetails.received;
+            $scope.failed = clientFileDetails.failed;
         });
     };
-    $scope.getFilesList = function (clientID) {
-        $http.get('clientList.json').success(function (client) {
-            $scope.files = client[clientID - 1].files;
-        });
-    };
+     $scope.getFilesList = function (clientID) {
+         ClientService.getFilesList().success(function (client) {
+             $scope.files = client[clientID - 1].files;
+         });
+     };
     $scope.getFileData = function (fName) {
-        $http.get('fileList.json').then(function (res) {
-            $scope.filesList = res.data;
-            for (i = 0; i < $scope.filesList.length; i++) {
-                var result = $scope.filesList[i].fileName;
-                if(fName === result){
-                    $scope.error = $scope.filesList[i].errors;
-                }
-            }
-        });
-    };
+         ClientService.getFileDetails().then(function (res) {
+             $scope.filesList = res.data;
+             for (i = 0; i < $scope.filesList.length; i++) {
+                 var result = $scope.filesList[i].fileName;
+                 if(fName === result){
+                     $scope.error = $scope.filesList[i].errors;
+                 }
+             }
+         });
+     };
 });
